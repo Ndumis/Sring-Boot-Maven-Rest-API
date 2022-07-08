61974c5903476bc141078ee1120cb850998464b6
Khaya <mkhayguze@gmail.com>
2022-07-08 21:03:25 +0200

    Added comment

--- restfulapi/src/main/java/com/restful/restfulapi/RestFulApiApplication.java
+++ restfulapi/src/main/java/com/restful/restfulapi/RestFulApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestFulApiApplication {
-
+	//Published to git
 	public static void main(String[] args) {
 		SpringApplication.run(RestFulApiApplication.class, args);
 	}
